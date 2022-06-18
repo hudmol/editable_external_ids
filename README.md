da342ca4cd5b0461c69130c11f1c573dcb82d016
James Bullen <jambun@gmail.com>
2022-06-18 12:33:00 +1000

    compatibility with AS v3.2.0

--- README.md
+++ README.md
@@ -1,12 +1,11 @@
+
 # editable_external_ids
-An ArchivesSpace plugin that adds the ability for Administrators to edit external ids
+
+An ArchivesSpace plugin that adds the ability for Administrators to edit
+external ids.
+
 
 ## How to install it
-
-First, you need to set the configuration to allow all users to see external ids in both
-record forms and readonly views:
-
-     AppConfig[:show_external_ids] = true
 
 To install, just activate the plugin in your config/config.rb file by
 including an entry such as:
@@ -21,7 +20,13 @@ ArchivesSpace plugins directory.  For example:
      cd /path/to/your/archivesspace/plugins
      git clone https://github.com/hudmol/editable_external_ids.git
 
-## v1.4+ Compatibility
+Or download and unpack your preferred version.
 
-This plugin is compatible with ArchivesSpace v1.4+. If you're after similar behaviour for
-v1.2 or v1.3, please have a look at our other plugin https://github.com/hudmol/visible_external_ids.
+
+## ArchivesSpace Version Compatibility
+
+This plugin was originally developed for ArchivesSpace v1.4+. If you are running
+an ArchivesSpace version earlier than v3.x then you should try version v0.2 of
+this plugin.
+
+If you are running version v3.x then use v1.0 of this plugin.
--- frontend/plugin_init.rb
+++ frontend/plugin_init.rb
@@ -1,24 +1,18 @@
 Rails.application.config.after_initialize do
 
-  ApplicationHelper.class_eval do
-
-    alias_method :render_aspace_partial_pre_editable_external_ids, :render_aspace_partial
-    def render_aspace_partial(args)
-
-      if args[:partial] == "external_ids/edit" && show_external_ids? && user_can?("administer_system")
-        return render :partial => "shared/subrecord_form", :locals => {:form => args[:locals][:form], :name => "external_ids"}
-      end
-
-      render_aspace_partial_pre_editable_external_ids(args);
-    end
-  end
-
-  SidebarHelper::SidebarGenerator.class_eval do
-    alias_method :show_external_ids_sidebar_entry_pre_editable_external_ids, :show_external_ids_sidebar_entry?
-    def show_external_ids_sidebar_entry?
-      show_external_ids_sidebar_entry_pre_editable_external_ids ||
-        (@form.controller.action_name != 'show' && @form.controller.class.session_can?(@form.controller, "administer_system"))
-    end
-  end
+  Plugins.register_plugin_section(
+    Plugins::PluginSubRecord.new(
+      'editable_external_ids',
+      'external_ids',
+      ['resource', 'archival_object', 'accession',
+       'collection_management', 'event', 'location', 'subject'],
+      {
+        template_name: 'external_id',
+        js_edit_template_name: 'template_external_id',
+        heading_text:  I18n.t('external_id._plural'),
+        sidebar_label:  I18n.t('external_id._plural'),
+      }
+    )
+  )
 
 end
